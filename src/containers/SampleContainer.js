8270233d4b394eba2621845d6c8b110d92210c12
rachel <rachel.uiux@gmail.com>
2023-11-30 20:59:01 +0900

    18.3.1.5 리팩토링

--- src/containers/SampleContainer.js
+++ src/containers/SampleContainer.js
@@ -13,8 +13,15 @@ const SampleContainer = ({
   loadingUsers,
 }) => {
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fn();
   }, [getPost, getUsers]);
   return (
     <Sample
@@ -27,11 +34,11 @@ const SampleContainer = ({
 };
 
 export default connect(
-  ({ sample }) => ({
+  ({ sample, loading }) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS,
+    loadingPost: loading["sample/GET_POST"],
+    loadingUsers: loading["sample/GET_USERS"],
   }),
   {
     getPost,
